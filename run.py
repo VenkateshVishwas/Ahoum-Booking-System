68e9c251c849917e7157a5ac51c4f6fdce6192d1
Venkatesh <venkateshvishwas12@gmail.com>
2025-07-03 15:21:37 +0530

    changes in requirments

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app, db
 from flask import Flask
+import os
 
 app = create_app()
 
@@ -8,4 +9,7 @@ if __name__ == '__main__':
         db.create_all()
         print("✅ Tables created!")
 
-    app.run(debug=True)
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
